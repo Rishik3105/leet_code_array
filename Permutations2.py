ffc6b747595d6b25da9182c6840dcd776cb9e87c
Nimmani Rishik <nimmanirishik@gmail.com>
2026-02-03 21:53:52 +0530

    Create Permutations2.py
    47. Permutations II
    Medium
    Topics
    premium lock icon
    Companies
    Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
    
     
    
    Example 1:
    
    Input: nums = [1,1,2]
    Output:
    [[1,1,2],
     [1,2,1],
     [2,1,1]]
    Example 2:
    
    Input: nums = [1,2,3]
    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]

---
+++ Permutations2.py
@@ -0,0 +1,34 @@
+def permuteUnique(nums):
+    nums.sort()          
+    result = []
+    used = [False] * len(nums)
+
+    def backtrack(path):
+        if len(path) == len(nums):
+            result.append(path[:])
+            return
+
+        for i in range(len(nums)):
+            # Skip used elements
+            if used[i]:
+                continue
+
+            # Skip duplicates
+            if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                continue
+
+            used[i] = True
+            path.append(nums[i])
+
+            backtrack(path)
+
+            path.pop()
+            used[i] = False
+
+    backtrack([])
+    return result
+
+
+if __name__ == "__main__":
+    nums = [1, 1, 2]
+    print(permuteUnique(nums))
