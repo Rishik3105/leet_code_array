fd63b02f6ab9e2c6cb57ea80c518d3b9660aac07
Nimmani Rishik <nimmanirishik@gmail.com>
2026-03-24 19:23:03 +0530

    Search in Rotated Sorted Array II.py

--- Search in Rotated Sorted Array II.py
+++ Search in Rotated Sorted Array II.py
@@ -1,12 +1,12 @@
-import array 
+import array
 
-def search(nums,target):
-    if target not in nums:
+def search(nums, target):
+    if target in nums:
         return True
     else:
         return False
- 
-if __name__=="__main__":
-    nums=array.array('i',map(int,input("Enter your sequence:").split()))
-    target=input("target:")
-    print(search(nums,target))
+        
+if __name__ == "__main__":
+    nums = array.array('i', map(int, input("Enter your sequence: ").split()))
+    target = int(input("Enter target: "))
+    print(search(nums, target))
