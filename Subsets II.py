0a6063c2cef30d4977e2b1ecf9e35677d10b1606
Nimmani Rishik <nimmanirishik@gmail.com>
2026-04-08 18:02:55 +0530

    Subsets II.py
    Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
    The solution set must not contain duplicate subsets. Return the solution in any order.
    
    Example 1:
    
    Input: nums = [1,2,2]
    Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
    Example 2:
    
    Input: nums = [0]
    Output: [[],[0]]

---
+++ Subsets II.py
@@ -0,0 +1,17 @@
+def subsetsWithDup(nums):
+    nums.sort()
+    res = []
+    def backtrack(start, path):
+        res.append(path[:])
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+    backtrack(0, [])
+    return res
+if __name__ == "__main__":
+    nums = list(map(int, input().split()))
+    result = subsetsWithDup(nums)
+    print(result)
