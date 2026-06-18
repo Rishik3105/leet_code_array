c6d8a8108ef84aa34c3a777b18124d19c3958f53
Nimmani Rishik <nimmanirishik@gmail.com>
2026-06-18 20:46:07 +0530

    Reconstruct Itinerary.py
    You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
    
    All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
    
    For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
    You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
    
     
    
    Example 1:
    
    
    Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
    Output: ["JFK","MUC","LHR","SFO","SJC"]
    Example 2:
    
    
    Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
    Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
    Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"] but it is larger in lexical order.

---
+++ Reconstruct Itinerary.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findItinerary(self, tickets):
+        graph = defaultdict(list)
+        for src, dst in sorted(tickets, reverse=True):
+            graph[src].append(dst)
+        itinerary = []
+        def dfs(airport):
+            while graph[airport]:
+                dfs(graph[airport].pop())
+            itinerary.append(airport)
+        dfs("JFK")
+        return itinerary[::-1]
